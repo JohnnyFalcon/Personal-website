8d9127b75ae9e8b3ccdd15ca9ac9191cb9297db4
Johnny <sokolowski8.kuba@gmail.com>
2023-01-26 00:54:59 +0100

    Add new project to Projects page

--- src/components/Projects/projectsData.js
+++ src/components/Projects/projectsData.js
@@ -3,20 +3,32 @@ import clothingappPhoto from "../../images/clothingapp-photo.png";
 import travelappPhoto from "../../images/travelapp-photo.png";
 import piggamePhoto from "../../images/piggame-photo.png";
 import calcPhoto from "../../images/calculator-photo.png";
+import codiblyPhoto from "../../images/codibly-photo.png";
 export const projects = [
   {
     id: 1,
-    title: "clothing website",
+    title: "Codibly task",
+    image: codiblyPhoto,
+    websiteLink: "https://phenomenal-custard-6fe128.netlify.app",
+    githubLink: "https://github.com/JohnnyFalcon/codibly-project",
+    color: "rgba(132, 132, 132, 0.2)",
+    new: true,
+    update: "NEW PROJECT",
+    date: "2023-01-25"
+  },
+  {
+    id: 2,
+    title: "Calculator website",
     image: calcPhoto,
     websiteLink: "https://phenomenal-tiramisu-0c4aef.netlify.app",
     githubLink: "https://github.com/JohnnyFalcon/calculator_app",
-    color: "rgba(134, 255, 251, 0.2)",
-    new: true,
+    color: "rgba(252, 172, 23, 0.2)",
+    new: false,
     update: "NEW PROJECT",
     date: "2022-12-12"
   },
   {
-    id: 2,
+    id: 3,
     title: "clothing website",
     image: clothingappPhoto,
     websiteLink: "https://regal-daifuku-70a96b.netlify.app",
@@ -27,7 +39,7 @@ export const projects = [
     date: "2022-12-13"
   },
   {
-    id: 3,
+    id: 4,
     title: "crypto website",
     image: cryptoappPhoto,
     websiteLink: "https://grand-praline-7b7928.netlify.app",
@@ -35,7 +47,7 @@ export const projects = [
     color: "rgba(250, 255, 147, 0.2)"
   },
   {
-    id: 4,
+    id: 5,
     title: "travel website ",
     image: travelappPhoto,
     websiteLink: "https://vocal-liger-0a7f4b.netlify.app",
@@ -43,7 +55,7 @@ export const projects = [
     color: "rgba(147, 255, 174, 0.2)"
   },
   {
-    id: 5,
+    id: 6,
     title: "Pig game website ",
     image: piggamePhoto,
     websiteLink: "https://thriving-paletas-fd36e4.netlify.app",
